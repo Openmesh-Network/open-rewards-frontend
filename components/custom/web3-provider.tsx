a806fd91caefc967063e02d156488d5f7fa514e7
Plopmenz <plopmenz@gmail.com>
2024-10-15 10:38:46 +0200

    remove polygon from chainlist

--- components/custom/web3-provider.tsx
+++ components/custom/web3-provider.tsx
@@ -4,11 +4,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { createWeb3Modal } from "@web3modal/wagmi/react"
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config"
 import { cookieStorage, createStorage, http, WagmiProvider } from "wagmi"
-import { mainnet, polygon, sepolia } from "wagmi/chains"
+import { mainnet, sepolia } from "wagmi/chains"
 
 import { siteConfig } from "@/config/site"
 
-export const chains = [mainnet, polygon, sepolia] as const
+export const chains = [mainnet, sepolia] as const
 export const defaultChain = mainnet
 
 const appName = siteConfig.name
@@ -33,7 +33,6 @@ const config = defaultWagmiConfig({
   }),
   transports: {
     [mainnet.id]: http("https://eth.llamarpc.com"),
-    [polygon.id]: http("https://polygon-rpc.com"),
     [sepolia.id]: http("https://rpc.sepolia.org"),
   },
   auth: { email: false },
